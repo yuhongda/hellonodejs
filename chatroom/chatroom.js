de476c5298d1e9d767b26d86777021bae9ebffbc
Yu Hongda <silverage.y@gmail.com>
2012-06-29 02:13:43 +0800

    it works.

---
+++ chatroom/chatroom.js
@@ -0,0 +1,24 @@
+﻿var http = require('http'),
+    io = require('socket.io'),
+    fs = require('fs');
+
+server = http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    var output = fs.readFileSync('./index.html', 'utf8');
+    response.end(output);
+});
+
+server.listen(8088);
+
+var socket = io.listen(server);
+socket.on('connection', function (client) {
+    client.broadcast.emit('message',{ message: client.id + ' is now available' });
+    client.on('message', function (msg) { 
+		console.log(msg.message+'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+		client.broadcast.emit('message',{ message: client.id + ': ' + msg.message });
+	});
+	client.on('announce1',function(anno){
+		client.broadcast.emit('announce',{message: anno.announcement });
+	});
+    client.on('disconnect', function () { client.broadcast.emit('message',{ message: client.id + ' is no longer available.' }) });
+});
